41267d3595742e1a4e3f514d5f861005dcd28dd3
Krystian Borowski <kdborowski@outlook.com>
2019-04-08 08:22:59 +0200

    FirstJSScript_timeline

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+document.getElementById("first").onclick = function() {myFunction()};
+
+function myFunction() {
+  var x = document.getElementById("text");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
